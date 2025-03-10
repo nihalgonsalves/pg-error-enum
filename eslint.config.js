d2e56d21874aa98bb19225aae97d01692ae7f60e
Nihal Gonsalves <nihal@nihalgonsalves.com>
2025-03-10 19:01:10 +0100

    build!: switch to esm

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import tseslint from "typescript-eslint";
+
+// eslint-disable-next-line import/extensions
+import sharedConfig from "@nihalgonsalves/esconfig/eslint.config.shared.js";
+
+export default tseslint.config(
+  { ignores: ["package-test", "dist"] },
+  ...sharedConfig,
+  {
+    rules: {
+      "@typescript-eslint/no-duplicate-enum-values": "off",
+      "@typescript-eslint/restrict-template-expressions": [
+        "error",
+        { allowNumber: true },
+      ],
+    },
+  },
+);
